94dece7c133c86c7dae4806d7008815b6190cad8
Kent <kentmoreland@gmail.com>
2016-10-30 12:02:01 -0500

    updated documentation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 let port = 8080;
 
+//db connection
 mongoose.connect('mongodb://localhost/jobster-t');
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -11,11 +12,29 @@ db.once('open', () => {
   console.log("we're connected!");
 });
 
-app.get('/', (req, res) => res.json({message: 'Welcome to Jobster!'}));
-
+//middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+//routes
+app.get('/', (req, res) => res.json({message: 'Welcome to Jobster!'}));
+
+app.route('/job')
+   .get(job.getJobs)
+   .post(job.postJob);
+
+app.route('/job/:id')
+   .get(Job.getJob)
+   .delete(Job.deleteJob)
+   .put(Job.updateJob);
+
+
+
+
+//server connect
 app.listen(port);
 console.log('listening on port ' + port);
+
+
+module.exports = app; //for testing
