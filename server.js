69c6a5a95e3e2562ad58883791f2c3f637e7a663
Kent <kentmoreland@gmail.com>
2016-10-31 15:33:52 -0500

    updated server root route

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.route('/api/job/:id')
 
 
 //frontend routes
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/views','index.html' ))
 })
 
