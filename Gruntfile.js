44b61da93f989bb65200889f417e04cd89d0ed71
Kent <kentmoreland@gmail.com>
2016-11-04 19:16:17 -0500

    added and configured grunt

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = (grunt) => {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    eslint: {
+      target: ['Gruntfile.js', 'public/js/**/*.js'],
+    },
+    watch: {
+      scripts: {
+        files: ['Gruntfile.js', 'public/*'],
+        tasks: ['eslint'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('gruntify-eslint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['eslint']);
+};
