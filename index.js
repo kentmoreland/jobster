8c7e7c5aa1ab514730cb13ac9810e6ad5d251a2b
Kent <kentmoreland@gmail.com>
2016-11-06 19:21:10 -0600

    added nodemon to the project and changed entry point to index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const job = require('./app/routes/job');
+
+const app = express();
+const port = 8080;
+
+// db connection
+mongoose.connect('mongodb://localhost/jobster-t');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("we're connected!");
+});
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// apiroutes
+app.get('/api', (req, res) => res.json({ message: 'Welcome to Jobster!' }));
+
+app.route('/api/job')
+   .get(job.getJobs)
+   .post(job.postJob);
+
+app.route('/api/job/:id')
+   .get(job.getJob)
+   .delete(job.deleteJob)
+   .put(job.updateJob);
+
+
+// frontend routes
+app.use(express.static(`${__dirname}/public`));
+
+// server connect
+app.listen(port);
+console.log(`Connect success! Listening on port ${port}.`);
+
+
+module.exports = app; // for testing
